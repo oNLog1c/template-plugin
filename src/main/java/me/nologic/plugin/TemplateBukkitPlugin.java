cf735dc5fcd37911edb4fbdbd0140fc3ac278f4b
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 09:46:00 +0600

    Initial commit

---
+++ src/main/java/me/nologic/plugin/TemplateBukkitPlugin.java
@@ -0,0 +1,19 @@
+package me.nologic.plugin;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public final class TemplateBukkitPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
